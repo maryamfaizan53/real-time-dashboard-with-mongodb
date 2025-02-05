216abdf89a795888606e3ca0a39bae64dae37606
maryamfaizan53 <maryam.faizanmianoor@gmail.com>
2025-02-05 18:41:06 +0500

    Update sanityClient.ts

--- src/app/api/sanity/sanityClient.ts
+++ src/app/api/sanity/sanityClient.ts
@@ -3,9 +3,9 @@
 import { createClient } from "next-sanity";
 
 export const sanityClient = createClient({
-  projectId: "",
+  projectId: "dys4b966",
   dataset:"production",
   apiVersion: "2023-10-01",
-  token: "",
+  token: "sk1ShR9WivKDD7Iu5OzVnMpTn6p40KX9AVJGeWwBCMuehE7p4wv4RRUbKRa4q6GFt0I1Q5zFnrrDJdVEtfzitBtN8p3xyzuVO32e9KMvrxE5XBUwZ4UhvGp9UheRbeUe4w8DDQ24150k2q38t4TVxcwX9P3cDxYNNBvp0QT9kovFNDouY0W1",
   useCdn: false,  
 });
